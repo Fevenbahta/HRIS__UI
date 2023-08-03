64f307feb0344fc2c40e2990dcdbe1eb650f753c
fevenbahta <fevenbahta994@gmail.com>
2023-08-03 17:13:20 +0300

    image

--- src/app/modules/nav/min-nav/min-nav.component.ts
+++ src/app/modules/nav/min-nav/min-nav.component.ts
@@ -15,4 +15,6 @@ export class MinNavComponent implements OnInit {
   ngOnInit(): void {
   }
 
+
+  
 }
--- src/app/modules/updatemodules/edit-employee/edit-employee.component.ts
+++ src/app/modules/updatemodules/edit-employee/edit-employee.component.ts
@@ -138,4 +138,27 @@ export class EditEmployeeComponent implements OnInit {
       }
     });
   }
+  openImageDialog(): void {
+    // Trigger click on the file input element to open the image dialog
+    const fileInput = document.getElementById('image') as HTMLInputElement;
+    fileInput.click();
+  }
+  onImageSelected(event: any): void {
+    if (event.target.files && event.target.files.length > 0) {
+      const file = event.target.files[0];
+      this.getBase64(file).then((data) => {
+        this.employeeForm.patchValue({
+          imageData: data
+        });
+      });
+    }
+  }
+    private getBase64(file: File): Promise<any> {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => resolve(reader.result);
+      reader.onerror = (error) => reject(error);
+    });
+  }
 }

db33e98f7d318515f2c2648dd408772b9104af7b
fevenbahta <fevenbahta994@gmail.com>
2023-08-18 16:51:04 +0300

    sav

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -9,17 +9,13 @@ export class HeaderComponent implements OnInit {
 
   @Output() toggleSideBarForMe: EventEmitter<any> = new EventEmitter();
 
+
   constructor() { }
 
   ngOnInit() { }
 
   toggleSideBar() {
     this.toggleSideBarForMe.emit();
-    setTimeout(() => {
-      window.dispatchEvent(
-        new Event('resize')
-      );
-    }, 300);
   }
 
 }

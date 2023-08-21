41b03e19048eae42298fa96c5cf21c1a3f77b339
fevenbahta <fevenbahta994@gmail.com>
2023-08-21 12:25:09 +0300

    MON

--- src/app/modules/Admin module/education-level/education-level.component.ts
+++ src/app/modules/Admin module/education-level/education-level.component.ts
@@ -10,7 +10,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-education-level',
   templateUrl: './education-level.component.html',
-  styleUrls: ['./education-level.component.scss']
+  styleUrls: ['./education-level.component.css']
 })
 export class EducationLevelComponent implements OnInit {
   educationLevels:EducationLevel[]=[];
